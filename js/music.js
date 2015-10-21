1699f87dbb204c130c3611b9dc7abacd1edcb845
Yi Pang <pangyibobo@gmail.com>
2015-10-21 10:01:20 -0700

    second

--- js/music.js
+++ js/music.js
@@ -37,8 +37,9 @@ $('form').submit(function() {
 		$(this).val('');
 	});
 	// After setting each property, save your new instance back to your database
-	song.save();
-	
+	music.save(null, {
+		success:getData
+	});
 	return false
 })
 
@@ -47,40 +48,64 @@ $('form').submit(function() {
 // Write a function to get data
 var getData = function() {
 	
-
+	console.log('getData');
 	// Set up a new query for our Music class
-
+	var query = new Parse.Query(Music);
 
 	// Set a parameter for your query -- where the website property isn't missing
-
+	//query.exists('website')
+	query.notEqualTo('website', '');
 
 	/* Execute the query using ".find".  When successful:
 	    - Pass the returned data into your buildList function
 	*/
+	query.find({
+		//success:function(response) {
+			//buildList(response)
+		//}
+		success: buildList
+	});
 }
 
 // A function to build your list
 var buildList = function(data) {
+	console.log('buildList', data);
 	// Empty out your unordered list
-	
+	$('ol').empty();
 	// Loop through your data, and pass each element to the addItem function
-
+	data.forEach(function(d) {
+		// body...
+		addItem(d);
+	})
 }
 
 
 // This function takes in an item, adds it to the screen
 var addItem = function(item) {
 	// Get parameters (website, band, song) from the data item passed to the function
-
+	console.log('addItem', item);
+	var website = item.get('website');
+	var band = item.get('bandName');
+	var song = item.get('bestSong');
 	
 	// Append li that includes text from the data item
-
-
+	var li = $('<li>check out ' + band + ' i love the song ' + song + ' </li> ');
+	var button = $('<button class="btn-xs btn-danger"><span class="glyphicon glyphicon-remove"></span></button>')
+	button.on('click', function() {
+		item.destroy({
+			success: getData
+		})
+	})
+	li.append(button)
+	$('ol').append(li);
 	
 	// Time pending, create a button that removes the data item on click
+	$('#remove').click(function() {
+		item.remove();
+	});
 	
 }
 
 // Call your getData function when the page loads
-
+getData();
 
